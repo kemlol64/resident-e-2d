8a69eb24c43cae87bdf4f1b1826d5c4092aea1e6
Kamel <kamel.tabti@etud.univ-pau.fr>
2026-06-14 16:21:14 -0600

    Movement Script 1.0 simple WASD

--- playerfrog.gd
+++ playerfrog.gd
@@ -1,25 +1,22 @@
 extends CharacterBody2D
+@onready var sprite2D = $AnimatedSprite2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
+@export var speed = 300
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	var original_scale=abs(sprite2D.scale.x)
+	var direction =Vector2.ZERO
+	if Input.is_action_pressed("left_walk"):
+		direction.x = -1
+		sprite2D.scale.x=original_scale*-1
+	if Input.is_action_pressed("right_walk"):
+		direction.x = 1
+		sprite2D.scale.x=original_scale*1
+	if Input.is_action_pressed("up_walk"):
+		direction.y = -1
+	if Input.is_action_pressed("down_walk"):
+		direction.y = 1
+	if direction.x != 0 or direction.y != 0:
+		direction=direction.normalized()
+	velocity=speed*direction
 	move_and_slide()
+		

a4b6a18ce127fe9b507b4bd3363679007d5905b6
Kamel <kamel.tabti@etud.univ-pau.fr>
2026-06-26 23:16:31 -0600

    changement structure de l'iventaire (Ajout inventaire Arme),Arme Class herite de Objet class

--- Armesresource.gd
+++ Armesresource.gd
@@ -1,9 +1,6 @@
-extends Resource
+extends Objet
 class_name Arme
 
-@export var icon: Texture2D
-@export var name: String
-@export var desc: String
 @export var Viseur: String
 @export var Damage: String
 @export var Cadence: String
--- case.gd
+++ case.gd
@@ -1,6 +1,9 @@
 extends Panel
 @onready var itemTexture=$TextureRect
 @onready var ItemQuantity=$Label
+enum slotType {ARME, OBJET }
+enum slotState {NONE,HOVERED,SELECTED}
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,7 +18,7 @@ func _process(delta: float) -> void:
 #dans la case si la case doit etre vide elle reste vide sinon ont y met l'icon de l'objet
 #ainsi que sa quantite dans label
 func set_item(item:Objet,index:int): #item objet a mettre dans la case , index c'est l'indes de la case (a voir utilite dans le script inventaire)
-	var case_index=index
+	var slot_index=index
 	if(item==null):#Si la case ne contient pas encore d'ojet
 		itemTexture.texture=null
 		ItemQuantity.text=""
